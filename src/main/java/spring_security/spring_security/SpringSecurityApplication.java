0f22c80febd75b446638def1842d167e0895dcd2
necati <necati.arslan2018@gmail.com>
2024-05-03 14:51:36 +0200

    it is done

--- src/main/java/spring_security/spring_security/SpringSecurityApplication.java
+++ src/main/java/spring_security/spring_security/SpringSecurityApplication.java
@@ -5,10 +5,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import spring_security.spring_security.Repository.PostRepository;
+import spring_security.spring_security.Repository.UserRepository;
 import spring_security.spring_security.model.Post;
+import spring_security.spring_security.model.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+
 
 @SpringBootApplication
 public class SpringSecurityApplication {
@@ -18,12 +24,15 @@ public class SpringSecurityApplication {
 	}
 
 	@Bean
-	CommandLineRunner commandLineRunner(PostRepository posts) {
+	CommandLineRunner commandLineRunner(PostRepository posts, UserRepository users, PasswordEncoder encoder) {
 		return args -> {
-
+			users.save(new User("user", encoder.encode("password"), "ROLE_USER"));
+			users.save(new User("admin",encoder.encode("password"),"ROLE_USER,ROLE_ADMIN"));
 			posts.save(new Post("Hello, World!","hello-world","Welcome to my new blog!","Dan Vega"));
 		};
 	}
+
+
 
 }
 
--- src/main/java/spring_security/spring_security/config/SecurityConfig.java
+++ src/main/java/spring_security/spring_security/config/SecurityConfig.java
@@ -2,13 +2,27 @@ package spring_security.spring_security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import spring_security.spring_security.UserDetailsService.JpaUserDetailsService;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    private final JpaUserDetailsService myUserDetailsService;
+
+    public SecurityConfig(JpaUserDetailsService myUserDetailsService) {
+        this.myUserDetailsService = myUserDetailsService;
+    }
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -17,8 +31,14 @@ public class SecurityConfig {
                 .authorizeRequests(auth -> auth
                 .requestMatchers("/h2-console/**").permitAll()
                 .requestMatchers("/api/posts/**").permitAll()
-                .anyRequest().authenticated()
-        )
+                .anyRequest().authenticated())
+                .userDetailsService(myUserDetailsService)
+                .httpBasic(withDefaults())
                 .build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
